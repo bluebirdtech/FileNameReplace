066c530fdcd7c63a128066523657a477066b9eb4
Justin Caldicott <justin@bluebirdtechnology.com>
2013-06-24 10:54:49 +0100

    Initial commit of FileNameReplacer

---
+++ FileNameReplace/FileNameReplacer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileNameReplace
+{
+    public class FileNameReplacer
+    {
+        private readonly string _filePath;
+        private readonly string _findText;
+        private readonly string _replaceText;
+
+        public FileNameReplacer( string filePath, string findText, string replaceText )
+        {
+            _filePath = filePath;
+            _findText = findText;
+            _replaceText = replaceText;
+        }
+
+        public void Execute()
+        {
+            var file = new FileInfo( _filePath );
+            string filename = Path.GetFileName( _filePath );
+            string directoryName = Path.GetDirectoryName( _filePath );
+            string newFilePath = Path.Combine( directoryName, filename.Replace( _findText, _replaceText ) );
+            file.MoveTo( newFilePath );
+        }
+
+        public string FilePath
+        {
+            get { return _filePath; }
+        }
+
+        public string FindText
+        {
+            get { return _findText; }
+        }
+
+        public string ReplaceText
+        {
+            get { return _replaceText; }
+        }
+    }
+}
---
+++ FileNameReplace/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileNameReplace
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var replacer = new FileNameReplacer( args[0], args[1], args[2] );
+            replacer.Execute();
+        }
+    }
+}
